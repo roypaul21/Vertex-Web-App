f9eb146c959020f8f836df95925a68f6e1fd4db1
roypnarido <roypnarido@gmail.com>
2024-06-24 23:09:16 +0800

    update home page

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -7,13 +7,13 @@ export default function HeroSection(){
 
             <div className="hero-bg-overlay"></div>
 
-            <div id="hero-content">
+            {/* <div id="hero-content">
                 <h1 className="text-white header-one">Credibility and <span className="d-block">Reliability for</span></h1>
                 <h1 className="text-white">Better Health Care</h1>
                 <a href="#footer-section">
                     <Button variant="light" className="mt-2">Get in Touch</Button>
                 </a>
-            </div>
+            </div> */}
         </section>
     )
 }
